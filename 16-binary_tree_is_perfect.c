c10421a65b71aabb2f957e64a38353338dac4d87
samisosa20 <1449@holbertonschool.com>
2020-07-21 20:45:06 +0000

    add if left and right is true

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -68,7 +68,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_is_leaf(tree))
 		return (1);
-
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
 	if (binary_tree_height(tree->left) ==
 		binary_tree_height(tree->right))
 	{

7de87d5d846f5a3bc63749db49479bb228f329b5
PabloYepes27 <juanpabloyepest@gmail.com>
2020-07-21 22:11:54 +0000

    Task 18 - update uncles

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,10 +9,16 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	if (node == NULL || node->parent == NULL ||
+	 node->parent->parent == NULL ||
+	 node->parent->parent->left == NULL ||
+	 node->parent->parent->right == NULL)
 		return (NULL);
 
-	if (node->parent->parent->left->left == node || node->parent->parent->left->right == node)
+
+
+	if (node->parent->parent->left->left == node ||
+	node->parent->parent->left->right == node)
 		return (node->parent->parent->right);
 	return (node->parent->parent->left);
 }
